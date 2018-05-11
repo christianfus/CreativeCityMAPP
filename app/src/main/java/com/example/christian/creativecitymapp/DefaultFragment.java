32169a8d1a53cc3c32667d02bcb3ef4bf723c657
christianfus <christianfus@gmail.com>
2018-05-11 15:23:52 +0100

    Added menus
    Menus added with more information about the app.

--- app/src/main/java/com/example/christian/creativecitymapp/DefaultFragment.java
+++ app/src/main/java/com/example/christian/creativecitymapp/DefaultFragment.java
@@ -1,37 +1,27 @@
 package com.example.christian.creativecitymapp;
 
-import android.app.FragmentTransaction;
+import android.app.Fragment;
 import android.content.Context;
-import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.TextView;
-
-
-import org.w3c.dom.Text;
 
 
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link ToolbarsFragment.OnFragmentInteractionListener} interface
+ * {@link DefaultFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link ToolbarsFragment#newInstance} factory method to
+ * Use the {@link DefaultFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class ToolbarsFragment extends Fragment {
+public class DefaultFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
-
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -39,7 +29,7 @@ public class ToolbarsFragment extends Fragment {
 
     private OnFragmentInteractionListener mListener;
 
-    public ToolbarsFragment() {
+    public DefaultFragment() {
         // Required empty public constructor
     }
 
@@ -49,78 +39,38 @@ public class ToolbarsFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment ToolbarsFragment.
+     * @return A new instance of fragment DefaultFragment.
      */
-
     // TODO: Rename and change types and number of parameters
-
-    public static ToolbarsFragment newInstance(String param1, String param2) {
-        ToolbarsFragment fragment = new ToolbarsFragment();
+    public static DefaultFragment newInstance(String param1, String param2) {
+        DefaultFragment fragment = new DefaultFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
-
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
-
-
-
-
     }
-
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_toolbars, container, false);
-        Button buy = (Button) view.findViewById(R.id.buybutton);
-
-        TextView place = (TextView)view.findViewById(R.id.infoplace);
-        TextView whats = (TextView)view.findViewById(R.id.infowhats);
-        TextView time = (TextView)view.findViewById(R.id.infotime);
-
-        String placetxt = getArguments().getString("PLACE");
-        String whatstxt = getArguments().getString("WHATS");
-        String timetxt = getArguments().getString("TIMES");
-
-            place.setText(placetxt);
-            whats.setText(whatstxt);
-            time.setText(timetxt);
-
-        buy.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                String url = "https://www.creativecity.org.uk/";
-
-                Intent i = new Intent(Intent.ACTION_VIEW);
-                i.setData(Uri.parse(url));
-                startActivity(i);
-            }
-        });
-
-        FragmentTransaction ft = getFragmentManager().beginTransaction();
-        ft.detach(this).attach(this).commit();
-
-        return view;
+        return inflater.inflate(R.layout.fragment_default, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
         if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
+            mListener.onFragmentInteractions(uri);
         }
     }
 
@@ -153,6 +103,6 @@ public class ToolbarsFragment extends Fragment {
      */
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+        void onFragmentInteractions(Uri uri);
     }
 }
--- app/src/main/java/com/example/christian/creativecitymapp/MapsActivity.java
+++ app/src/main/java/com/example/christian/creativecitymapp/MapsActivity.java
@@ -3,6 +3,7 @@ package com.example.christian.creativecitymapp;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
@@ -10,8 +11,13 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.res.ResourcesCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -22,7 +28,7 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, ToolbarsFragment.OnFragmentInteractionListener{
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback, ToolbarsFragment.OnFragmentInteractionListener, DefaultFragment.OnFragmentInteractionListener{
 
     private GoogleMap mMap;
 
@@ -31,9 +37,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
+
         mapFragment.getMapAsync(this);
 
     }
@@ -53,33 +62,57 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
 
         mMap = googleMap;
-        Marker marker_1;
 
         TextView text1 = (TextView)findViewById(R.id.Timetable);
 
         LatLng manchester = new LatLng(53.483959, -2.244644);
         LatLng manchester2 = new LatLng(53.485979, -2.244634);
 
-        Drawable myDrawable = ResourcesCompat.getDrawable(getResources(), R.drawable.pin2, null);
+        Drawable myDrawable = ResourcesCompat.getDrawable(getResources(), R.drawable.pins, null);
         Bitmap markerBitmap = ((BitmapDrawable)myDrawable).getBitmap();
 
-        Marker m = mMap.addMarker(new MarkerOptions().icon(BitmapDescriptorFactory.fromBitmap(markerBitmap)).position(manchester2).title("Dance Performance - CHORLTON VILLAGE SQR."));
-        mMap.addMarker(new MarkerOptions().icon(BitmapDescriptorFactory.fromBitmap(markerBitmap)).position(manchester).title("MacBeth - EDGE THEATER"));
+        Marker MDance = mMap.addMarker(new MarkerOptions().icon(BitmapDescriptorFactory.fromBitmap(markerBitmap)).position(manchester2).title("Dance Performance - CHORLTON VILLAGE SQR."));
+        Marker MTheater = mMap.addMarker(new MarkerOptions().icon(BitmapDescriptorFactory.fromBitmap(markerBitmap)).position(manchester).title("MacBeth - EDGE THEATER"));
 
         float zoomLevel = 15.0f; //This goes up to 21
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(manchester, zoomLevel));
 
-        FragmentManager manager = getFragmentManager();
+        final FragmentManager manager = getFragmentManager();
 
-        ToolbarsFragment fragtools = new ToolbarsFragment();
+        final ToolbarsFragment fragtools = new ToolbarsFragment();
+        final DefaultFragment fragdefault = new DefaultFragment();
+
         manager.beginTransaction()
-                .replace(R.id.toolbarLayout, fragtools, fragtools.getTag())
+                .replace(R.id.toolbarLayout, fragdefault, fragdefault.getTag())
                 .commit();
 
+        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
 
-        onMarkerClick(m);
+            public boolean onMarkerClick(Marker marker) {
 
+                Bundle toolbarbundle = new Bundle();
 
+                if (marker.getTitle().equals("Dance Performance - CHORLTON VILLAGE SQR.")) {
+                    toolbarbundle.putString("PLACE", "Chorlton Village Sqr.");
+                    toolbarbundle.putString("WHATS", "Dance Performance");
+                    toolbarbundle.putString("TIMES", "19/05 - 11:00PM");
+
+                }
+                if (marker.getTitle().equals("MacBeth - EDGE THEATER")) {
+                    toolbarbundle.putString("PLACE", "Edge Theater");
+                    toolbarbundle.putString("WHATS", "MacBeth Performance");
+                    toolbarbundle.putString("TIMES", "13/05 - 9:00PM");
+
+                }
+
+                fragtools.setArguments(toolbarbundle);
+
+                transactFragment(fragtools,true);
+
+                return false;
+            }
+        });
 
     }
 
@@ -87,8 +120,15 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * handle marker click event
      */
 
-    public void onMarkerClick(Marker marker) {
-            Log.w("Click", "test");
+    public void transactFragment(Fragment fragment, boolean reload) {
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        if (reload) {
+            getSupportFragmentManager().popBackStack();
+        }
+        transaction.replace(R.id.toolbarLayout, fragment);
+        transaction.addToBackStack(null);
+        transaction.detach(fragment).attach(fragment).commit();
     }
 
     public void showHideFragment(final Fragment fragment){
@@ -108,9 +148,33 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         fragTransaction.commit();
     }
 
+    public boolean onCreateOptionsMenu (Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.activity_menu, menu);
+        return true;
+    }
+    public boolean onOptionsItemSelected (MenuItem item){
+        switch (item.getItemId()){
+            case R.id.inform:
+                Intent acerca = new Intent(MapsActivity.this, InfoActivity.class);
+                startActivity(acerca);
+                return true;
+            case R.id.about:
+                Intent pedido = new Intent(MapsActivity.this, AboutActivity.class);
+                startActivity(pedido);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 
     @Override
     public void onFragmentInteraction(Uri uri) {
+
+    }
+
+    public void onFragmentInteractions(Uri uri) {
 
     }
 }
